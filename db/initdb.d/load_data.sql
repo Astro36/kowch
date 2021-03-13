3af8c9bdd6937c0888dd0b36c3a7e754f8be688c
Seungjae Park <astro.psj@gmail.com>
2021-03-13 19:57:43 +0900

    Set enclosed character on data loader

--- db/initdb.d/load_data.sql
+++ db/initdb.d/load_data.sql
@@ -1,5 +1,5 @@
-LOAD DATA INFILE './languages.csv' INTO TABLE languages FIELDS TERMINATED BY ',' (`id`, `description`);
-LOAD DATA INFILE './pos.csv' INTO TABLE pos FIELDS TERMINATED BY ',' (`id`, `description`);
-LOAD DATA INFILE './kinds.csv' INTO TABLE kinds FIELDS TERMINATED BY ',' (`id`, `description`);
-LOAD DATA INFILE './categories.csv' INTO TABLE categories FIELDS TERMINATED BY ',' (`id`, `description`);
+LOAD DATA INFILE './languages.csv' INTO TABLE languages FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (`id`, `description`);
+LOAD DATA INFILE './pos.csv' INTO TABLE pos FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (`id`, `description`);
+LOAD DATA INFILE './kinds.csv' INTO TABLE kinds FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (`id`, `description`);
+LOAD DATA INFILE './categories.csv' INTO TABLE categories FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (`id`, `description`);
 
